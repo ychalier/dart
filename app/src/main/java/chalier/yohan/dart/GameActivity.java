19368962f735030b008ff9846b3454405970251d
ychalier <yohan.chalier@telecom-paristech.fr>
2018-05-02 16:33:04 +0200

    better design for game activity & game end implementation

--- app/src/main/java/chalier/yohan/dart/GameActivity.java
+++ app/src/main/java/chalier/yohan/dart/GameActivity.java
@@ -7,10 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Comparator;
 
-    private int playerCount;
-    private int startScore;
+public class GameActivity extends AppCompatActivity {
 
     private DartPickerFragment picker1;
     private DartPickerFragment picker2;
@@ -18,10 +18,17 @@ public class GameActivity extends AppCompatActivity {
 
     private TextView playerLabel;
     private TextView scoreLabel;
-    private TextView scoreboard;
+    private TextView scoreboardNames;
+    private TextView scoreboardScores;
+    private TextView rankLabel;
 
+    private int playerCount;
+    private int playerUnfinished;
+    private int turn;
     private int[] scores;
+    private int[] results;
     private int currentPlayer;
+    private String[] playerNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +37,19 @@ public class GameActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         playerCount = intent.getIntExtra("playerCount", 2);
-        startScore = intent.getIntExtra("startScore", 301);
+        int startScore = intent.getIntExtra("startScore", 301);
+        playerUnfinished = playerCount;
 
         scores = new int[playerCount];
+        results = new int[playerCount];
+        playerNames = new String[playerCount];
         for (int i = 0; i < scores.length; i++) {
             scores[i] = startScore;
+            results[i] = -1;
+            playerNames[i] = Integer.toString(i + 1);
         }
 
+        turn = 0;
         currentPlayer = -1;
 
         picker1 = (DartPickerFragment) getSupportFragmentManager()
@@ -46,12 +59,14 @@ public class GameActivity extends AppCompatActivity {
         picker3 = (DartPickerFragment) getSupportFragmentManager()
                         .findFragmentById(R.id.fragmentDartPicker3);
 
-        playerLabel = findViewById(R.id.textView2);
-        scoreLabel = findViewById(R.id.textView3);
-        scoreboard = findViewById(R.id.textView4);
+        playerLabel = findViewById(R.id.textViewPlayerName);
+        scoreLabel = findViewById(R.id.textViewPlayerScore);
+        rankLabel = findViewById(R.id.textViewPlayerRank);
+        scoreboardNames = findViewById(R.id.textViewScoreboardNames);
+        scoreboardScores = findViewById(R.id.textViewScoreboardScores);
 
         final GameActivity self = this;
-        Button button = findViewById(R.id.button2);
+        Button button = findViewById(R.id.buttonNextPlayer);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,30 +83,96 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private void update() {
-        if (currentPlayer >= 0) {
+
+        // Computing score of current player
+        if (currentPlayer >= 0 && scores[currentPlayer] > 0) {
             int score = evaluate();
             if (scores[currentPlayer] - score >= 0) {
                 scores[currentPlayer] -= score;
             }
             if (scores[currentPlayer] == 0) {
-                Intent intent = new Intent(this, ResultsActivity.class);
-                intent.putExtra("scores", scores);
-                startActivity(intent);
+                playerUnfinished--;
+                results[currentPlayer] = turn;
+
+                // Disable NEXT button when everything is over
+                // It prevents the user from starting an infinite loop
+                if (playerUnfinished == 0) {
+                    findViewById(R.id.buttonNextPlayer).setEnabled(false);
+                }
             }
         }
-        currentPlayer = (currentPlayer + 1) % playerCount;
-        playerLabel.setText("Player " + (currentPlayer + 1));
-        scoreLabel.setText(Integer.toString(scores[currentPlayer]));
+
+        // Find next player who has not finished yet
+        // The condition is always true or never entered
+        while (playerUnfinished > 0) {
+            currentPlayer = (currentPlayer + 1) % playerCount;
+            if (currentPlayer == 0) {
+                turn++;
+            }
+            if (scores[currentPlayer] > 0) {
+                break;
+            }
+        }
 
         picker1.reset();
         picker2.reset();
         picker3.reset();
 
-        String scoreboardText = "";
+        playerLabel.setText(playerNames[currentPlayer]);
+        scoreLabel.setText(String.format("%03d", scores[currentPlayer]));
+
+        setTitle(String.format("Turn %d", turn));
+
+        ArrayList<Integer> ranks = new ArrayList<>();
         for (int i = 0; i < playerCount; i++) {
-            scoreboardText += "Player " + (i + 1) + ": " + scores[i] + "\n";
+            ranks.add(i);
         }
-        scoreboard.setText(scoreboardText);
+
+        ranks.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer integer, Integer t1) {
+
+                if (results[integer] > -1 && results[t1] == -1) {
+                    return -1;
+                } else if (results[integer] == -1 && results[t1] > -1) {
+                    return 1;
+                } else if (results[integer] == -1 && results[t1] == -1) {
+                    if (scores[integer] > scores[t1]) {
+                        return 1;
+                    } else if (scores[integer] < scores[t1]) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                } else {
+                    if (results[integer] > results[t1]) {
+                        return 1;
+                    } else if (results[integer] < results[t1]) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                }
+
+            }
+        });
+
+        StringBuilder scoreboardNamesText = new StringBuilder();
+        StringBuilder scoreboardScoresText = new StringBuilder();
+        for (int index : ranks) {
+            scoreboardNamesText.append("Player " + (index + 1) + "\n");
+
+            if (results[index] == -1) {
+                scoreboardScoresText.append(String.format("%03d\n", scores[index]));
+            } else {
+                scoreboardScoresText.append(String.format("Done in %d turns\n", results[index]));
+            }
+
+        }
+        scoreboardNames.setText(scoreboardNamesText.toString());
+        scoreboardScores.setText(scoreboardScoresText.toString());
+
+        rankLabel.setText(String.format("%d", ranks.indexOf(currentPlayer) + 1));
     }
 
 }
